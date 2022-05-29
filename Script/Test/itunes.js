20e8326a3056ac7d484e5e0cbb610eb598e6a620
vemodalen <vemodalen@163.com>
2022-05-30 00:38:25 +0800

    测试noted4

--- Script/Test/itunes.js
+++ Script/Test/itunes.js
@@ -18,7 +18,7 @@ obj["latest_receipt_info"][0]["is_in_intro_offer_period"] = "false";
 obj["latest_receipt_info"][0]["in_app_ownership_type"] = "PURCHASED";
 */
 
-if (bundle_id == "net.shinyfrog.bear-iOS") {
+if (bundle_id == "com.digitalworkroom.noted") {
   obj ={
     "environment": "Production",
     "receipt": {
